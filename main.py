d39b54a856f6e2b9e05ef573da9e00f5c432bd90
Recrucity <68477164+Recrucity@users.noreply.github.com>
2022-04-02 22:30:40 +0200

    update v1.1

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 # Made by Recrucity#4040
+# TODO: Embeds say how often profit reports are sent // DONE
+# TODO: Style improvements. (Code blocks over robux profit etc) // DONE
+# TODO: Add Mutli-Account Support (Maybe difficult, Probably not that difficult)
 
 import requests
 import configparser
 import os
 import time
+from colorama import init
+
+init()
 
 # Config
 config = configparser.ConfigParser()
 config.read_file(open(r"config.ini"))
+timeType = config.get("main", "time")
 checkTime = int(config.get("main", "checktime"))
-userId = str(config.get("main", "userid"))
+userId = config.get("main", "userid")
 
 # Discord
 webhook = str(config.get("discord", "webhook"))
@@ -22,14 +29,16 @@ while True:
     itemAmount = len(data)
     userRap = 0
 
+    print(colorama.Fore.CYAN + "CHECKING " + colorama.Fore.RESET + "Checking your limiteds...")
+
     for i in range(itemAmount):
         itemData = data[i]
         itemRap = itemData["recentAveragePrice"]
-        print("RAP: " + str(itemRap))
 
         userRap = userRap + itemRap
-
-    print("Total RAP: " + str(userRap))
+        print(
+            colorama.Fore.CYAN + "CHECKED " + colorama.Fore.RESET + " Checked limited #" + str(i + 1) + " (RAP: " + str(
+                itemRap) + ")")
 
     if os.path.exists("lastday.txt"):
         f = open("lastday.txt", "r")
@@ -48,19 +57,23 @@ while True:
 
     if userRap > int(lastDay):
         profit = f'{userRap - int(lastDay):,}'
-        message = "**You've made profit since Last Time!**\nTotal Profit: " + str(profit) + " R$"
+        message = "You've made profit since Last Time!\nTotal Profit: `" + str(profit) + " R$`\nFrom `" + str(f'{lastDay:,}')\
+                  + " R$` to `" + str(f'{userRap:,}') + " R$`\n \nNext profit report will be sent in "\
+                  + str(checkTime) + " " + str(timeType)
         color = 65353
     elif userRap < int(lastDay):
         loss = f'{int(lastDay) - userRap}'
-        message = "**You've lost RAP Since Last Time**\nTotal Loss: " + str(loss) + " R$"
+        message = "You've lost RAP since last time!\nTotal Loss: `" + str(loss) + " R$`\nFrom `" + str(f'{lastDay:,}')\
+                  + " R$` to `" + str(f'{userRap:,}') + " R$`\n \nNext profit report will be sent in "\
+                  + str(checkTime) + " " + str(timeType)
         color = 16711680
     else:
-        message = "**Your RAP Has not Changed Since Last Time**"
+        message = "Your RAP Has not Changed Since Last Time"
         color = 16777215
 
     data = {
-        "content" : topmessage,
-        "username" : "Profit Reporter"
+        "content": topmessage,
+        "username": "Profit Reporter"
     }
 
     data["embeds"] = [
@@ -71,11 +84,21 @@ while True:
         }
     ]
     requests.post(webhook, json=data)
+    print(colorama.Fore.LIGHTGREEN_EX + "SUCCESS " + colorama.Fore.RESET + "Posted Profit Report to Discord!")
 
     f = open("lastday.txt", "w")
     f.write(str(userRap))
     f.close()
 
-    time.sleep(checkTime)
+    print(colorama.Fore.LIGHTBLUE_EX + "WAITING " + colorama.Fore.RESET + "Waiting for " + str(
+        checkTime) + " " + timeType)
+    if timeType == "seconds":
+        time.sleep(checkTime)
+    elif timeType == "minutes":
+        time.sleep(checkTime * 60)
+    elif timeType == "hours":
+        time.sleep(checkTime * 3600)
+    elif timeType == "days":
+        time.sleep(checkTime * 86400)
 
 # Made by Recrucity#4040
